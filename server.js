fbb3cfae778bdc561e5649a88076b9ba12ac3301
Micah Bales <micahbales@gmail.com>
2017-11-12 18:43:18 -0500

    Add Styles & Ready Server for Production
    • Add color scheme, logo, and style details
    • Update server to work properly in production
    • Change CDNs to https, to avoid Mixed Content errors

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const serveStatic = require('serve-static');
 /* For production, serve static files from root */
 app.use(serveStatic(__dirname));
 
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8000, () => {
   console.log('Express Server Listening on Port 8000');
 });
