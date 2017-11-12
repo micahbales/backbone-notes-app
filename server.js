c722f845a69a46cc049e4f5390bd0cbf12467795
Micah Bales <micahbales@gmail.com>
2017-11-12 10:19:53 -0500

    Initial Commit
    • Create basic index template and style sheet
    • Import Backbone dependencies via CDN
    • Create a simple Express server for loading the app in production

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const serveStatic = require('serve-static');
+
+/* For production, serve static files from root */
+app.use(serveStatic(__dirname));
+
+app.listen(8000, () => {
+  console.log('Express Server Listen on Port 8000');
+});
